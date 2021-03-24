5c7d99d9ce3e448623d76049794e8201f8eca95c
Uriel Alvarez Valdez <azaki14@gmail.com>
2021-03-23 20:35:51 -0600

    Agregados contenedores necesarios de tagbar

--- src/components/TagsBar/TagsBar.js
+++ src/components/TagsBar/TagsBar.js
@@ -1,19 +1,40 @@
-import React, { useEffect } from "react"
+import React from "react"
 import PropTypes from "prop-types"
 
-const TagsBar = ({ tags }) => {
-  useEffect(() => console.log("cambiaron los tags"), [tags])
-  return (
-    <div className="tags-bar">
-      {tags.map(({ id, tag }) => (
-        <li key={id} className="tag-in=bar">
-          {tag}
-        </li>
-      ))}
+const TagsBar = ({ tags, handleRemoveTag, handleRemoveAllTags }) => (
+  <>
+    <div className="tags-bar wrapper">
+      <div className="tags-container">
+        {tags.map(({ id, tag }) => (
+          <div className="tag-wrapper" key={id}>
+            <li className="tag-in-bar" id={id}>
+              {tag}
+            </li>
+            <button
+              className="remove-tag"
+              type="button"
+              onClick={() => handleRemoveTag(id)}
+            >
+              X
+            </button>
+          </div>
+        ))}
+      </div>
+      <div className="clearAllContainer">
+        <button
+          className="clear-all"
+          type="button"
+          onClick={() => handleRemoveAllTags()}
+        >
+          Clear All
+        </button>
+      </div>
     </div>
-  )
-}
+  </>
+)
 TagsBar.propTypes = {
   tags: PropTypes.arrayOf(PropTypes.object).isRequired,
+  handleRemoveTag: PropTypes.func.isRequired,
+  handleRemoveAllTags: PropTypes.func.isRequired,
 }
 export default TagsBar
